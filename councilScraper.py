53a52373717b11dbd364b147227a11f655b41d8d
Bogil, John <John.Bogil@viacommix.com>
2015-12-17 18:24:07 -0500

    Email scraping not working yet

--- councilScraper.py
+++ councilScraper.py
@@ -4,18 +4,31 @@ import re
 
 from BeautifulSoup import BeautifulSoup
 
-url = 'http://council.nyc.gov/d17/html/members/home.shtml'
+url = 'http://council.nyc.gov/d51/html/members/home.shtml'
 response = requests.get(url)
 html = response.content
 
 soup = BeautifulSoup(html)
-td = soup.find('td', attrs={'class' : 'nav_text'})
-
-text = td.text.replace('&nbsp;', '')
-
-phoneWithTitle = re.search("((Legislative Office Phone)+.{12})", text)
-phone = "".join(re.findall('\d+', phoneWithTitle.group(1)))
+tdNavText = soup.find('td', attrs={'class' : 'nav_text'})
 
 
+text = tdNavText.text.replace('&nbsp;', '')
 
-print phone
+
+def getEmail():
+	tdNavText = soup.find('td', attrs={'class' : 'nav_text'})
+	print tdNavText
+	email = re.findall("\w+", tdNavText.text)
+	print email
+
+def getLegislativePhone():
+	legislativePhoneWithTitle = re.search("((Legislative Office Phone)+.{12})", text)
+	legislativePhone = "".join(re.findall('\d+', legislativePhoneWithTitle.group(0)))
+	print legislativePhone
+
+def getDistrictPhone():
+	districtPhoneWithTitle = re.search("((District Office Phone)+.{12})", text)
+	districtPhone = "".join(re.findall('\d+', districtPhoneWithTitle.group(0)))
+	print districtPhone
+
+getEmail()
