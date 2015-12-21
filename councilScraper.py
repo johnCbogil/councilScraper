bfe393dd4f2eb7d8b2cca19faa00c71ab3bff9b6
Bogil, John <John.Bogil@viacommix.com>
2015-12-21 13:38:18 -0500

    Now iterating through all 51 council members for email and phone. Next step is to load into csv

--- councilScraper.py
+++ councilScraper.py
@@ -5,27 +5,38 @@ import bs4
 
 from BeautifulSoup import BeautifulSoup
 
-url = 'http://council.nyc.gov/d51/html/members/home.shtml'
-response = requests.get(url)
-html = response.content
-soup = BeautifulSoup(html)
+districtNumber = 1
 
-tdNavText = soup.find('td', attrs={'class' : 'nav_text'})
-text = tdNavText.text.replace('&nbsp;', '')
+while districtNumber <= 51:
 
-def getLegislativePhone():
-	legislativePhoneWithTitle = re.search("((Legislative Office Phone)+.{12})", text)
-	legislativePhone = "".join(re.findall('\d+', legislativePhoneWithTitle.group(0)))
-	print legislativePhone
+	url = 'http://council.nyc.gov/d%d/html/members/home.shtml' % (districtNumber)
+	print url
+	response = requests.get(url)
+	html = response.content
+	soup = BeautifulSoup(html)
 
-def getDistrictPhone():
-	districtPhoneWithTitle = re.search("((District Office Phone)+.{12})", text)
-	districtPhone = "".join(re.findall('\d+', districtPhoneWithTitle.group(0)))
-	print districtPhone
+	tdNavText = soup.find('td', attrs={'class' : 'nav_text'})
+	text = tdNavText.text.replace('&nbsp;', '')
 
-def getMailto():
-	for a in tdNavText.findAll('a'):
-		if 'mailto' in a['href']:
-  			print a.get('href')[7:]
+	def getLegislativePhone():
+		legislativePhoneWithTitle = re.search("((Legislative Office Phone)+.{12})", text)
+		legislativePhone = "".join(re.findall('\d+', legislativePhoneWithTitle.group(0)))
+		print legislativePhone
 
-getMailto()
+	def getDistrictPhone():
+		districtPhoneWithTitle = re.search("((District Office Phone)+.{12})", text)
+		districtPhone = "".join(re.findall('\d+', districtPhoneWithTitle.group(0)))
+		print districtPhone
+
+	def getMailto():
+		for a in tdNavText.findAll('a'):
+			if 'mailto' in a['href']:
+	  			email = a.get('href')[7:]
+	  			if email:
+	  				print email
+	  			else: 
+	  				print "no email found"
+	  		
+	getMailto()
+
+	districtNumber += 1
