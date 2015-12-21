d0e356a9c28357c9bfee6710a0bf34486bcca441
Bogil, John <John.Bogil@viacommix.com>
2015-12-21 13:13:57 -0500

    Emails are now being properly scraped

--- councilScraper.py
+++ councilScraper.py
@@ -1,25 +1,17 @@
 import csv
 import requests
 import re
+import bs4
 
 from BeautifulSoup import BeautifulSoup
 
 url = 'http://council.nyc.gov/d51/html/members/home.shtml'
 response = requests.get(url)
 html = response.content
-
 soup = BeautifulSoup(html)
+
 tdNavText = soup.find('td', attrs={'class' : 'nav_text'})
-
-
 text = tdNavText.text.replace('&nbsp;', '')
-
-
-def getEmail():
-	tdNavText = soup.find('td', attrs={'class' : 'nav_text'})
-	print tdNavText
-	email = re.findall("\w+", tdNavText.text)
-	print email
 
 def getLegislativePhone():
 	legislativePhoneWithTitle = re.search("((Legislative Office Phone)+.{12})", text)
@@ -31,4 +23,9 @@ def getDistrictPhone():
 	districtPhone = "".join(re.findall('\d+', districtPhoneWithTitle.group(0)))
 	print districtPhone
 
-getEmail()
+def getMailto():
+	for a in tdNavText.findAll('a'):
+		if 'mailto' in a['href']:
+  			print a.get('href')[7:]
+
+getMailto()
